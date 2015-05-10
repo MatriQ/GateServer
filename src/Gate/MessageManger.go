2f15c5b27b94ef53d196f0f71d1c6d22262ae454
Matrixdom <matrixdom@126.com>
2015-05-10 15:45:52 +0800

    add Messager and Hander interface

--- src/Gate/MessageManger.go
+++ src/Gate/MessageManger.go
@@ -1,5 +1,18 @@
 package Gate
 
+import (
+	"Gate/NetSession"
+)
+
+type Messager interface {
+	GetSession() *NetSession.NetSession
+}
+
+type Hander interface {
+	Action()
+	GetMessage() *Messager
+}
+
 type MessageManager struct {
 	Handers  [int]interface{}
 	Messages [int]interface{}
@@ -10,11 +23,17 @@ func NewMessagePool() {
 	return ret
 }
 
-func (*MessageManager) Register(id int, handler interface{}, message interface{}) {
+func (manager *MessageManager) register(id int, handler *Hander, message *Messager) {
+	manager.Handers[id] = handler
+	manager.Messages[id] = message
 }
 
-func (*MessageManager) CreateHandler(id int) interface{} {
+func (manager *MessageManager) CreateHandler(id int) *Hander {
+	var ret = &Hander{}
+	return ret
 }
 
-func (*MessageManager) CreateMessage(id int) interface{} {
+func (manager *MessageManager) CreateMessage(id int) *Messager {
+	var ret = &Messager{}
+	return ret
 }
