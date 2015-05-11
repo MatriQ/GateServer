5e2ace961945cbd3e4a5d31b88adf7b55aecfc5a
Matrixdom <matrixdom@126.com>
2015-05-11 23:01:50 +0800

    Modify UserLoginHandler

--- src/Gate/User/Handlers/UserLoginHander.go
+++ src/Gate/User/Handlers/UserLoginHander.go
@@ -1,14 +1,16 @@
 package Handlers
 
 import (
+	"Gate"
 	"Gate/Manager"
 )
 
 type UserLoginHander struct {
+	message *Gate.Messager
 }
 
-func (hander *UserLoginHander) GetMessage() {
-	return nil
+func (hander *UserLoginHander) GetMessage() *Gate.Messager {
+	return hander.message
 }
 
 func (hander *UserLoginHander) Action() {
